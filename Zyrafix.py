a1802e6409f277b05795cea21a05cc208a3f0f76
Jakub Ignatowicz <window231@interia.pl>
2019-03-25 19:59:37 +0100

    Update #1

--- Zyrafix.py
+++ Zyrafix.py
@@ -1,2 +1,20 @@
-import pygame
+import pygame, sys
 from pygame.locals import *
+
+pygame.init()
+resolution = (1280, 720)
+window = pygame.display.set_mode(resolution, DOUBLEBUF)
+pygame.display.set_caption('Zyrafix')
+#tlo=pygame.image.load()
+
+screen = pygame.display.get_surface()
+
+def input(events):
+    for event in events:
+        if event.type == QUIT:
+            sys.exit(0)
+        else:
+            print(event)
+
+while True:
+    input(pygame.event.get())
