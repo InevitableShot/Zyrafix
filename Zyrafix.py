8660000a6eb72122e11a230fd91fc23d7f276f4b
Piotrek <pi.kusnierz@gmail.com>
2019-04-01 12:34:18 +0200

    #dziala

--- Zyrafix.py
+++ Zyrafix.py
@@ -6,21 +6,26 @@ pygame.init()
 resolution = (1280, 720)
 window = pygame.display.set_mode(resolution, DOUBLEBUF)
 pygame.display.set_caption('Achtung die Kurve')
-tlo=pygame.image.load('playnow.png')
+play=pygame.image.load('playnow.png')
 wyjscie=pygame.image.load('exit.png')
-# kappa
-# kappav2
+#tlo
 screen = pygame.display.get_surface()
-screen.blit(tlo,(500,200))
+screen.blit(play,(500,200))
 screen.blit(wyjscie,(500,350))
 pygame.display.flip()
-
+play_pos = Rect(500,200,226,75)
 
 def input(events):
     for event in events:
         if event.type == QUIT:
             sys.exit(0)
-        else:
-            print(event)
+        if event.type == MOUSEBUTTONDOWN:
+            mouse_pos = event.pos
+            if pygame.mouse.get_pressed()[0] and play_pos.collidepoint(mouse_pos):
+                print("Hi")
+        """else:
+            print(event)"""
 while True:
     input(pygame.event.get())
+
+
