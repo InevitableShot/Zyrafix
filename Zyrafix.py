1c6821fa5abf858e54c77b689c732dd4ea28afb6
Piotrek <pi.kusnierz@gmail.com>
2019-04-01 11:16:03 +0200

    #dodane napisy

--- Zyrafix.py
+++ Zyrafix.py
@@ -6,9 +6,13 @@ pygame.init()
 resolution = (1280, 720)
 window = pygame.display.set_mode(resolution, DOUBLEBUF)
 pygame.display.set_caption('Achtung die Kurve')
-# tlo=pygame.image.load()
+tlo=pygame.image.load('playnow.png')
+wyjscie=pygame.image.load('exit.png')
 # kappa
 screen = pygame.display.get_surface()
+screen.blit(tlo,(500,200))
+screen.blit(wyjscie,(500,350))
+pygame.display.flip()
 
 
 def input(events):
