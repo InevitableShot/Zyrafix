4a83ad3d3eb91ab352c857c9364fdeaac4c8c4b6
Piotrek <pi.kusnierz@gmail.com>
2019-04-03 10:07:12 +0200

    #dziala

--- Zyrafix.py
+++ Zyrafix.py
@@ -14,15 +14,18 @@ screen.blit(play,(500,200))
 screen.blit(wyjscie,(500,350))
 pygame.display.flip()
 play_pos = Rect(500,200,226,75)
+wyjscie_pos = Rect(500,350,226,75)
 
 def input(events):
     for event in events:
-        if event.type == QUIT:
-            sys.exit(0)
+        #if event.type == EXIT:
+            #sys.exit(0)
         if event.type == MOUSEBUTTONDOWN:
             mouse_pos = event.pos
             if pygame.mouse.get_pressed()[0] and play_pos.collidepoint(mouse_pos):
                 print("Hi")
+            elif pygame.mouse.get_pressed()[0] and wyjscie_pos.collidepoint(mouse_pos):
+                sys.exit(0)
         """else:
             print(event)"""
 while True:
